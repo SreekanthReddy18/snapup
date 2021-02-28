5b7984cf77d123399991d83473aed6ea3c5ca238
SreekanthReddy18 <sreekanth3143@gmail.com>
2021-02-28 11:11:51 +0530

    Initial commit.

---
+++ src/test/java/com/snapup/snapup/SnapupApplicationTests.java
@@ -0,0 +1,14 @@
+/*package com.snapup.snapup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
